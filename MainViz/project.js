c2ce6759f2a3ccbee83c43faaca392a31b3596ef
CozyBurrito <cozyburrito@gmail.com>
2019-04-09 22:04:40 -0600

    Add tooltip

--- MainViz/project.js
+++ MainViz/project.js
@@ -8,7 +8,7 @@
 /**
  * Call our functions on window load event
  */
-window.onload = function(){
+window.onload = function () {
     setup();
 };
 
@@ -16,13 +16,13 @@ window.onload = function(){
  * object to keep track of our magic numbers for margins
  * @type {{top: number, left: number, bottom: number, right: number}}
  */
-const MARGINS = {top: 40, right: 10, bottom: 100, left: 160};
+const MARGINS = { top: 40, right: 10, bottom: 100, left: 160 };
 
 const CATEGORIES = ["Thriller", "Mystery", "Sci-Fi", "Biography", "Horror", "Fantasy", "Drama", "Crime", "Comedy", "Animation", "Adventure", "Action"];
 
 const COLORS = ["#C3F5CD", "#AAEFC5", "#91E9C5", "#7AE3CA", "#63DBD6", "#4CC0D3", "#369ACB", "#2A5DAC", "#1F2B8C", "#25166B"];
 
-var Scatterplot = function(){
+var Scatterplot = function () {
     this.data;          // contains our dataset
     this.width = 800;   // default value of 1000
     this.height = 750;  // default value of 700
@@ -47,10 +47,10 @@ var Scatterplot = function(){
      * @param yRange (required) array containing the bounds of the interval we are scaling to (e.g., [0,100]) for the y-axis
      * @param yDomain (required) array containing the bounds of the interval from which our input comes from (e.g., [0,1] for the y-axis
      */
-    this.setupScales = function(xRange, xDomain, yRange, yDomain){
+    this.setupScales = function (xRange, xDomain, yRange, yDomain) {
         this.xAxisScale = d3.scaleLog()
-             .domain(xDomain).nice()
-             .range(xRange).nice();
+            .domain(xDomain).nice()
+            .range(xRange).nice();
 
         this.yAxisScale = d3.scalePoint()
             .domain(yDomain)
@@ -58,19 +58,19 @@ var Scatterplot = function(){
 
         // Size of circle is the films Runtime
         this.rScale = d3.scaleLinear()
-            .domain(d3.extent(this.data, function (d) { return +d["Runtime"]; } )) 
-            .range([8,20]);
+            .domain([60, 180])
+            .range([8, 20]);
 
         // Color represents average rating
         this.fillScale = d3.scaleQuantile()
-            .domain(d3.extent(this.data, function (d) { return +d["AvgRating"]; } ))
+            .domain([0, 100])
             .range(COLORS);
     };
 
     /**
      * Function setupAxes initializes D3 axes for our Scatterplot
      */
-    this.setupAxes = function(){
+    this.setupAxes = function () {
         xLabel = "Revenue (Millions)";
         yLabel = "Genre";
 
@@ -82,13 +82,13 @@ var Scatterplot = function(){
             .tickPadding(10);
         // call d3's axisLeft for the y-axis
         this.yAxis = d3.axisLeft(this.yAxisScale)
-            .tickSize(-this.width + MARGINS.left*2)
+            .tickSize(-this.width + MARGINS.left * 2)
             .ticks(10)
             .tickPadding(15);
 
         // call our axes inside "group" (<g></g>) objects inside our SVG container
         this.svgContainer.append("g")
-            .attr("transform", `translate(0, ${this.height - MARGINS.bottom })`)
+            .attr("transform", `translate(0, ${this.height - MARGINS.bottom})`)
             .call(this.xAxis);
         this.svgContainer.append("g")
             .attr("transform", `translate(${MARGINS.left}, 0)`)
@@ -99,13 +99,13 @@ var Scatterplot = function(){
         // add text labels
         this.svgContainer.append("text")
             .attr("x", MARGINS.left)
-            .attr("y", (this.height)/2)
-            .attr("transform", `rotate(-90, ${MARGINS.left / 3}, ${this.height/2})`)
+            .attr("y", (this.height) / 2)
+            .attr("transform", `rotate(-90, ${MARGINS.left / 3}, ${this.height / 2})`)
             .style("text-anchor", "middle")
             .attr("id", "labelText")
             .text(yLabel);
         this.svgContainer.append("text")
-            .attr("x", (this.width)/2)
+            .attr("x", (this.width) / 2)
             .attr("y", (this.height - MARGINS.top))
             .style("text-anchor", "middle")
             .attr("id", "labelText")
@@ -118,46 +118,100 @@ var Scatterplot = function(){
      * @param xAxisSelector the data property for values to appear in x-axis
      * @param yAxisSelector the data property for values to appear in y-axis
      */
-    this.createCircles = function(xAxisSelector, yAxisSelector){
+    this.createCircles = function (xAxisSelector, yAxisSelector) {
         // default to Life Satisfaction and Employment Rate
         xAxisSelector = "Revenue";
         yAxisSelector = "Genre";
 
+        var tooltip = d3
+            .componentsTooltip()
+            .container(this.svgContainer)
+            .content([
+                {
+                    left: "Title: ",
+                    right: "{title}"
+                },
+                {
+                    left: "Genre: ",
+                    right: "{genre}"
+                },
+                {
+                    left: "Director: ",
+                    right: "{director}"
+                },
+                {
+                    left: "Runtime (Min): ",
+                    right: "{runtime}"
+                },
+                {
+                    left: "Revenue (Millions): ",
+                    right: "{revenue}"
+                },
+                {
+                    left: "Average Rating: ",
+                    right: "{avgRating}"
+                }
+            ]);
+
         // Use D3's selectAll function to create instances of SVG:circle virtually
         // per item in our data array
         this.datapoints = this.svgContainer.selectAll("circle")
-            .data(this.data)    
-            .enter()            
-            .append("circle")   
-            .attr("r", function(d){                 // Scale radius based off of film's Runtime
+            .data(this.data)
+            .enter()
+            .append("circle")
+            .attr("r", function (d) {                 // Scale radius based off of film's Runtime
                 return _vis.rScale(d["Runtime"]);
             })
-            .attr("cx", function(d){
+            .attr("cx", function (d) {
                 return _vis.xAxisScale(d[xAxisSelector]);
             })
-            .attr("cy", function(d){
+            .attr("cy", function (d) {
                 return _vis.yAxisScale(d[yAxisSelector]);
             })
-            .style("fill", function(d){             // Color represents average rating
+            .style("fill", function (d) {             // Color represents average rating
                 return _vis.fillScale(d["AvgRating"]);
             })
-            .append("svg:title")
-            .text(function(d){
-                return "Title: " + d.Title + "\n" 
-                        + "Genre: " + d.Genre + "\n" 
-                        + "Director: " + d.Director + "\n" 
-                        + "Runtime (Min): " + d.Runtime + "\n" 
-                        + "Revenue (Millions): " + d.Revenue + "\n" 
-                        + "AvgRating: " + d.AvgRating;
+            .on("mouseenter", (d, i) => {
+                var xloc = _vis.xAxisScale(d[xAxisSelector]);
+                var yloc = _vis.yAxisScale(d[yAxisSelector]) - 20;
+                var direction = "bottom";
+                var fill = _vis.fillScale(d["AvgRating"]);
+
+                if (yloc < this.height/4) {
+                    direction = "top";
+                    yloc += 40;
+                }
+
+                tooltip
+                    .textColor("white")
+                    .tooltipFill(fill)
+                    .fontSize(14)
+                    .x(xloc)
+                    .y(yloc)
+                    .direction(direction)
+                    .leftMargin(3)
+                    .rightMargin(13)
+                    .contentMargin(5)
+                    .heightOffset(8)
+                    .show({ title: d.Title, 
+                            genre: d.Genre, 
+                            director: d.Director, 
+                            runtime: d.Runtime, 
+                            revenue: d.Revenue, 
+                            avgRating: d.AvgRating
+                        });
+            })
+            .on("mouseleave", (d, i) => {
+                tooltip.hide();
             });
     }
 
     // Setup the legends from the scales. Uses d3-legend
-    this.setupLegends = function(){
+    this.setupLegends = function () {
         // The quantile color legend
         this.svgContainer.append("g")
             .attr("class", "legendQuant")
-            .attr("transform", "translate(" + (_vis.width-MARGINS.left+20) + "," + (MARGINS.top+12) + ")");
+            .attr("transform", "translate(" + (_vis.width - MARGINS.left + 20) + "," + (MARGINS.top + 12) + ")");
 
         var quantileLegend = d3.legendColor()
             .labelFormat(d3.format(".2f"))
@@ -171,7 +225,7 @@ var Scatterplot = function(){
         // The linear size legend
         this.svgContainer.append("g")
             .attr("class", "legendSize")
-            .attr("transform", "translate(" + (_vis.width-MARGINS.left+20) + "," + (MARGINS.top+292) + ")");
+            .attr("transform", "translate(" + (_vis.width - MARGINS.left + 20) + "," + (MARGINS.top + 292) + ")");
 
         var legendSize = d3.legendSize()
             .scale(_vis.rScale)
@@ -197,7 +251,7 @@ var _vis;               // our visualization
 /**
  * Function setup: sets up our visualization environment.
  */
-function setup(){
+function setup() {
     _vis = new Scatterplot();
     _vis.svgContainer = d3.select("#vis");
     // dynamically change our SVG container's dimensions with the current browser dimensions
@@ -215,13 +269,13 @@ function setup(){
  * Function loadData: loads data from a given CSV file path/url
  * @param path string location of the CSV data file
  */
-function loadData(path){
+function loadData(path) {
     // call D3's loading function for CSV and load the data to our global variable _data
-    d3.csv(path).then(function(data){
+    d3.csv(path).then(function (data) {
         _vis.data = data;
         // let's use the scales and domain from Life Satisfaction and Employment Rate
-         _vis.setupScales([MARGINS.left, _vis.width-MARGINS.left], [1e-2, 950],
-             [_vis.height-MARGINS.bottom-30, MARGINS.top], CATEGORIES);
+        _vis.setupScales([MARGINS.left, _vis.width - MARGINS.left], [1e-2, 950],
+            [_vis.height - MARGINS.bottom - 30, MARGINS.top], CATEGORIES);
         _vis.setupAxes();
         _vis.createCircles();
         _vis.setupLegends();
